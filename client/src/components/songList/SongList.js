00cd3e45f00bc16f744604124f76fdb2262aa65f
rkochary <razmikkocharian@gmail.com>
2024-01-21 20:23:08 +0400

    add playing song in footer

--- client/src/components/songList/SongList.js
+++ client/src/components/songList/SongList.js
@@ -6,37 +6,46 @@ import PlayingSong from '../playingSong/PlayingSong';
 const SongList = () => {
 
     const [songList,setSongList] = useState(data)
-    const [playingSong,setPlayingSong] = useState(null);
+    //const [playingSong,setPlayingSong] = useState(null);
 
-    // const findPlayingSong = () => {
-    //     setPlayingSong(songList.filter(song => song.isPlaying))
+    const playingSong = songList.find(song => song.isPlaying)
+
+
+    const handleIsPlaying = (id) => {
+        const updatedSongs = songList.map((song) =>
+      song.trackNumber === id ? { ...song, isPlaying: !song.isPlaying } : { ...song, isPlaying: false }
+    );
+
+    setSongList(updatedSongs); 
+    }
+
+
+    // const handleIsPlaying = (id) => {
+        
+    //     setSongList((prev) => prev.map((song) => {
+    //         if(song.trackNumber === id){
+    //             return {
+    //                 ...song,
+    //                     isPlaying:!song.isPlaying
+    //             }
+    //         } else {
+    //             return song
+    //         }
+    //     }
+    //     ))
     // }
 
 
 
-    const handleIsPlaying = (id) => {
-        
-        setSongList((prev) => prev.map((song) => {
-            if(song.trackNumber === id){
-                return {...song,
-                        isPlaying:!song.isPlaying
-                }
-            } else {
-                return song
-            }
-        }
-        ))
-    }
-
-
-
-    console.log(songList);
+    console.log(playingSong);
 
   return (
     <div>
         {songList.map((song) => 
              <SongRow key={song.trackNumber} id={song.trackNumber} handleIsPlaying={handleIsPlaying} songName={song.songName} isPlaying={song.isPlaying} artist={song.artistName} i={song.trackNumber}/>
         )}
+        {playingSong?<div>{playingSong.songName}</div> : null}
+        
         {/* <PlayingSong song={playingSong} /> */}
     </div>
   )
